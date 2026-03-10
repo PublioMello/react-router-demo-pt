2c59b36c78b4e2dd89f447936db0abced115968a
Publio Filho <publio.lima.mello@gmail.com>
2026-03-10 20:45:05 -0300

    Rotas aninhadas

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,19 @@
+import { Link,Outlet } from "react-router-dom";
 import "./AboutUs.css";
 
 function AboutUs() {
   return (
     <div className="about-us">
+    <ul>
+      <li>
+        <Link to="site-history">Nossa Historia</Link>
+      </li>
+      <li>
+        <Link to="site-mission">Nossa Missao</Link>
+      </li>
+      </ul>
       <p>Encontre mais informações sobre nosso site aqui.</p>
+      <Outlet/>
     </div>
   );
 }
